23224e75d07a0c7e150b8a21468c54b6ba88ec37
Zahid Chandio <zahidalichandio240@gmail.com>
2023-10-05 11:03:12 +0500

    port updated.

--- start.js
+++ start.js
@@ -1,7 +1,9 @@
 // Config file to launch our application
 
-const app = require('./app');
+const app = require("./app");
 
-const server = app.listen(3000, () => {
-    console.log(`Express app running on port ${server.address().port}`)
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Express app running on port ${server.address().port}`);
 });
